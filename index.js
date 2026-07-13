919a93cb4c11ac3123326b2976f97c1fff8bb7eb
wesamnahedamassi <wesam199455@gmail.com>
2026-07-13 03:16:36 -0700

    feat: setup react native navigation

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-/**
- * @format
- */
+import { startApp } from './src/navigation/AppNavigator'
 
-import { AppRegistry } from 'react-native';
-import App from './App';
-import { name as appName } from './app.json';
+startApp()
 
-AppRegistry.registerComponent(appName, () => App);
+
+
+
+
+
+
---
+++ src/navigation/AppNavigator.tsx
@@ -0,0 +1,42 @@
+import { Navigation } from "react-native-navigation";
+import ProductList from "../screens/ProductList";
+
+
+import { QueryClientProvider } from "@tanstack/react-query";
+import { queryClient } from "../services/query_client";
+import { ProductDetails } from "../screens/ProductDetails";
+
+const ProductListWithProvider = (prop: { componentId: string }) => {
+    return <QueryClientProvider client={queryClient}>
+        <ProductList componentId={prop.componentId} />
+
+    </QueryClientProvider>
+}
+
+
+
+export function startApp() {
+    Navigation.registerComponent('ProductList', () => ProductListWithProvider)
+    Navigation.registerComponent('ProductDetails', () => ProductDetails)
+
+
+    Navigation.events().registerAppLaunchedListener(() => {
+        Navigation.setRoot({
+            root: {
+                stack: {
+                    children: [
+                        {
+                            component: {
+                                name: 'ProductList', options: {
+                                    topBar: { title: { text: 'Products' } }
+                                }
+                            },
+                        }
+                    ]
+                }
+            }
+        })
+    })
+
+
+}
